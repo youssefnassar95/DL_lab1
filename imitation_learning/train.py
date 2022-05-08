b41366fdb7073c91728a7140e429121e0d6cacba
youssefnassar95 <93321141+youssefnassar95@users.noreply.github.com>
2022-05-08 13:25:21 +0200

    Update train.py

--- imitation_learning/train.py
+++ imitation_learning/train.py
@@ -139,7 +139,7 @@ def train_model(X_train, y_train, X_valid, y_valid, n_minibatches, batch_size, l
 
     # TODO: specify your agent with the neural network in agents/bc_agent.py 
     agent = BCAgent(lr=lr)
-    agent.to(device)
+#     agent.to(device)
     
     tensorboard_eval = Evaluation(tensorboard_dir, 'evaluation')
 
@@ -155,7 +155,7 @@ def train_model(X_train, y_train, X_valid, y_valid, n_minibatches, batch_size, l
     # training loop
     for i in range(n_minibatches):
         X_batch, y_batch = sample_minibatch(X_train, y_train, batch_size)
-        outputs, y_batch, loss = agent.update(X_batch, y_batch)
+        outputs, y_batch, loss = agent.update(X_batch.to(device), y_batch.to(device))
 
         _, predicted = torch.max(torch.abs(outputs).detach(), 1)
         _, targetsbinary = torch.max(torch.abs(y_batch).detach(), 1)
