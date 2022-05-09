b24cd166aa0c13e0e92a30c4c15e5dac68e94ecc
youssefnassar95 <93321141+youssefnassar95@users.noreply.github.com>
2022-05-10 01:01:59 +0200

    Add files via upload

--- imitation_learning/train.py
+++ imitation_learning/train.py
@@ -172,7 +172,7 @@ def train_model(X_train, y_train, X_valid, y_valid, n_minibatches, batch_size, l
 
     # TODO: specify your agent with the neural network in agents/bc_agent.py
     agent = BCAgent(lr=lr)
-    tensorboard_eval = Evaluation(tensorboard_dir, "trani-1", stats=["train_acc", "valid_acc", "loss"])
+    tensorboard_eval = Evaluation(tensorboard_dir, "train-1", stats=["train_acc", "valid_acc", "loss"])
     # TODO: implement the training
     #
     # 1. write a method sample_minibatch and perform an update step
